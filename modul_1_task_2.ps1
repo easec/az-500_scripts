6e1d3883da04d7a8b7f933d84c67a3a1866f8e64
easec <mats.johannesson@easec.se>
2021-03-28 09:53:43 +0200

    Update modul_1_task_2.ps1

--- modul_1_task_2.ps1
+++ modul_1_task_2.ps1
@@ -1,5 +1,5 @@
 
-Connect-AzureAD
+Connect-AzAccount
 $Email = Read-Host "Skriv in e-post för testkonto"
 
 $userpassword = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
@@ -7,18 +7,18 @@ $userpassword.Password = "Pa55w.rd1234"
 
 
 $domainName = ((Get-AzureAdTenantDetail).VerifiedDomains)[0].Name
+  
+New-AzADUser -DisplayName 'Marcus Rosenberg' -GivenName 'Marcus' -Surname 'Rosenberg' -PasswordProfile $userpassword -UserPrincipalName "MarcusR@$domainName" -AccountEnabled $true -MailNickName 'Marcus'
+New-AzADUser -DisplayName 'Viking Johannesson' -GivenName 'Viking' -Surname 'Johannesson' -PasswordProfile $userpassword -UserPrincipalName "VikingJ@$domainName" -OtherMails $Email -AccountEnabled $true -MailNickName 'Viking'
 
-New-AzureADUser -DisplayName 'Marcus Rosenberg' -GivenName 'Marcus' -Surname 'Rosenberg' -PasswordProfile $userpassword -UserPrincipalName "MarcusR@$domainName" -AccountEnabled $true -MailNickName 'Marcus'
-New-AzureADUser -DisplayName 'Viking Johannesson' -GivenName 'Viking' -Surname 'Johannesson' -PasswordProfile $userpassword -UserPrincipalName "VikingJ@$domainName" -OtherMails $Email -AccountEnabled $true -MailNickName 'Viking'
 
-
-New-AzureADGroup -Description "AZ500Admins" -DisplayName "AZ500Admins" -MailEnabled $false -SecurityEnabled $true -MailNickName "AZ500Admins"
+New-AzADGroup -Description "AZ500Admins" -DisplayName "AZ500Admins" -MailEnabled $false -SecurityEnabled $true -MailNickName "AZ500Admins"
 
 $ObjectGroup = Get-AzureADGroup -SearchString "AZ500Admins"
 
-$ObjectIdUser1 = Get-AzureADUser -Filter "GivenName eq 'Marcus'"
-$ObjectIdUser2 = Get-AzureADUser -Filter "GivenName eq 'Viking'"
+$ObjectIdUser1 = Get-AzADUser -Filter "GivenName eq 'Marcus'"
+$ObjectIdUser2 = Get-AzADUser -Filter "GivenName eq 'Viking'"
 
 
-Add-AzureADGroupMember -ObjectId $ObjectGroup.ObjectId -RefObjectId $ObjectIdUser1.ObjectId
-Add-AzureADGroupMember -ObjectId $ObjectGroup.ObjectId -RefObjectId $ObjectIdUser2.ObjectId
+Add-AzADGroupMember -ObjectId $ObjectGroup.ObjectId -RefObjectId $ObjectIdUser1.ObjectId
+Add-AzADGroupMember -ObjectId $ObjectGroup.ObjectId -RefObjectId $ObjectIdUser2.ObjectId
